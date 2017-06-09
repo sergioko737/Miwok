e37181a9b343d3b2590b891d63374c10c3f3d49b
sergioko737 <s29585476@gmail.com>
2017-06-09 05:29:55 +0300

    Add Word constructor with image + reference to images in Numbers, Colors, Family .java

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -2,7 +2,7 @@ package com.example.android.miwok;
 
 /**
  * {@link Word} represents a vocabulary word that the user wants to learn.
- * It contains a default translation and a Miwok translation for that word.
+ * It contains a default translation and a Miwok translation, and an image for that word.
  */
 
 public class Word {
@@ -12,6 +12,9 @@ public class Word {
     /** Miwok translation for the word */
     private String mMiwokTranslation;
 
+    /** Image resource ID for the word  */
+    private int mImageResourceId;
+
     /**
      *  Create a new Word object.
      *  @param defaultTranslation is the word in a language that the user is already familiar with
@@ -19,6 +22,20 @@ public class Word {
      *  @param miwokTranslation is the word in the Miwok language
      */
     public Word(String defaultTranslation, String miwokTranslation){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     *  Create a new Word object.
+     *  @param defaultTranslation is the word in a language that the user is already familiar with
+     *                            (such as English)
+     *  @param miwokTranslation is the word in the Miwok language
+     *
+     *  @param imageResourceId is the drawable resource ID for the image ass
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId){
+        mImageResourceId = imageResourceId;
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
     }
@@ -35,5 +52,10 @@ public class Word {
      */
     public String getMiwokTranslation(){
         return mMiwokTranslation;
+    }
+
+    /** Return the image resource ID of the word */
+    public int getmImageResourceId() {
+        return mImageResourceId;
     }
 }
